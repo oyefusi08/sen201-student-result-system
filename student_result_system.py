936e696544219b07be3df3ad49905bc0f2bdaa23
oyefusi08 <oluwaferanmioyefusi@gmail.com>
2026-01-21 22:25:24 +0100

    Create student_result_system.py

---
+++ student_result_system.py
@@ -0,0 +1,8 @@
+student_name = input("enter student name: ")
+student_score = int(input("Enter student score: "))
+
+print("Student Name:", student_name)
+print("Student Score:", student_score)
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Try programiz.pro")
